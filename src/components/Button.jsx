45eddb1fcec8351b7de1210c7918b2cffe8c8930
the-soban <sobanshafeq123@gmail.com>
2023-08-06 23:38:12 +0500

    styled buttons section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
+const getButtonType = btn => {
+    const className = {
+        '=': 'equals',
+        'x': 'opt-button',
+        '-': 'opt-button',
+        '+': 'opt-button',
+        '/': 'opt-button',
+        'AC': 'fn-button',
+        '+-': 'fn-button',
+        '%': 'fn-button',
+    }
+    return className[btn];
+}
+
 const Button = ({ value }) => {
-    return <button className="button">{value}</button>
+    return <button className={`${getButtonType(value)} button`}>{value}</button>
 }
 
 export default Button
