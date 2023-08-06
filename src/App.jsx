9c822a2c50a9bb454391763fcdd75b59a7ae6a0e
the-soban <sobanshafeq123@gmail.com>
2023-08-06 20:10:16 +0500

    created wrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Wrapper from './components/Wrapper'
 
 function App() {
     return (
         <>
-            <h1>Hello World</h1>
+            <Wrapper>
+                <h1>Hello World</h1>
+            </Wrapper>
         </>
     )
 }
